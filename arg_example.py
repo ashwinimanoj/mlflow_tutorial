9395c679cf409c71cbbc242362120d36d24ce08d
Ashwini Manoj <ashwinimanoj@gmail.com>
2023-09-23 11:22:39 +0530

    linear regression v0 for wine quality

---
+++ arg_example.py
@@ -0,0 +1,11 @@
+import sys
+
+args = sys.argv
+
+print(args)
+
+alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
+l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+
+print(alpha)
+print(l1_ratio)
